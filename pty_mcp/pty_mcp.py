049ce8ae8e464a115876561ed6c29b3494ab183b
David Wurtz <david.w@qodo.ai>
2025-04-30 12:35:04 -0700

    abs path for log

--- pty_mcp/pty_mcp.py
+++ pty_mcp/pty_mcp.py
@@ -9,7 +9,7 @@ from mcp.server.fastmcp import FastMCP
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-file_handler = logging.FileHandler('pty_mcp.log')
+file_handler = logging.FileHandler('/tmp/pty_mcp.log')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
